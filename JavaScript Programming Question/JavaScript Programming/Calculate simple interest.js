95f5d0c87654ea9202c054070782219a4c936a10
KJR-dev <sahooj168@gmail.com>
2023-04-29 21:14:33 +0530

    own pratices

---
+++ JavaScript Programming Question/JavaScript Programming/Calculate simple interest.js
@@ -0,0 +1,12 @@
+// Calculate simple interest
+// simpleInterest=(principalAmaount*time*rateOfInterest)/100;
+// principalAmaount
+// time
+// rateOfInterest
+
+function simpleInterest(principalAmaount, time, rateOfInterest) {
+    return (principalAmaount*time*rateOfInterest)/100;
+}
+
+console.log(simpleInterest(16000,2,10));
+ 
---
+++ JavaScript Programming Question/JavaScript Programming/HelloWorld.js
@@ -0,0 +1,2 @@
+console.log("Hello world!");
+console.log("i am jitendra sahoo");
