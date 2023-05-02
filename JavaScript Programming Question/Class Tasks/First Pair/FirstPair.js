3d87dd613aa16e5ad158c56f3e62de8710d03ea1
KJR-dev <sahooj168@gmail.com>
2023-05-02 12:44:29 +0530

    doc add

--- JavaScript Programming Question/Class Tasks/First Pair/FirstPair.js
+++ JavaScript Programming Question/Class Tasks/First Pair/FirstPair.js
@@ -1,5 +1,15 @@
 //To find a first pair from a number array whose sum is X
 
+/**
+ * The function finds the first pair of numbers in an array that add up to a given sum.
+ * @param inputArray - [1, 2, 3, 4, 5, 6]
+ * @param sum - The sum parameter is the target sum that we want to find a pair of numbers that add up
+ * to it. In the example code, it is set to 4.
+ * @returns The function `firstPair` returns an object with a `pairs` property that contains an array
+ * of two numbers that add up to the given `sum` if such a pair exists in the `inputArray`. If no such
+ * pair exists, the function returns `false`.
+ */
+
 function firstPair(inputArray,sum){
     for (let index1 = 0; index1 < inputArray.length-1; index1++) {
         for (let index2 = index1+1; index2 < inputArray.length; index2++) {
