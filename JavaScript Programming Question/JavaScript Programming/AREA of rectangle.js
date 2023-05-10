728ca54173018cde32bda551fb66c924e2d38fa6
KJR-dev <sahooj168@gmail.com>
2023-05-11 04:41:45 +0530

    Program to calculate the AREA of rectangle 1.0

---
+++ JavaScript Programming Question/JavaScript Programming/AREA of rectangle.js
@@ -0,0 +1,12 @@
+// Program to calculate the AREA of rectangle
+//Step 1: call to area function and pass lenght and breadth
+//Step 2: define area function defination 
+// Step 3: return area value
+//Step 4: print output
+function area(x,y) {
+    return x*y;
+}
+
+let lenght=10;
+let breadth=20;
+console.log(area(lenght,breadth));
